18af6df76949847428c0910f6f830c3abec484e8
sajid-islam <sajidislam941@gmail.com>
2026-04-07 22:23:22 +0600

    chore: add Prettier configuration and integrate Tilt Warp font support

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,23 @@
-import { satoshi } from "./fonts";
-import "./globals.css";
+import { Tilt_Warp } from 'next/font/google';
+import { satoshi } from './fonts';
+
+import './globals.css';
+
+const tiltWarp = Tilt_Warp({ weight: ['400'], subsets: ['latin'], variable: '--font-tilt-warp' });
 
 export const metadata = {
-    title: "Gymify | Transform Your Body, Elevate Your Life",
-    description:
-        "Gymify is your ultimate fitness destination. Explore workouts, training programs, and expert tips to build strength, stay fit, and achieve your goals.",
-    icons: {
-        icon: "/logo.png",
-    },
+  title: 'Gymify | Transform Your Body, Elevate Your Life',
+  description:
+    'Gymify is your ultimate fitness destination. Explore workouts, training programs, and expert tips to build strength, stay fit, and achieve your goals.',
+  icons: {
+    icon: '/logo.png ',
+  },
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en" className={`${satoshi.className} h-full antialiased`}>
-            <body className="min-h-full flex flex-col">{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en" className={`${satoshi.className} ${tiltWarp.variable} h-full antialiased`}>
+      <body className="flex min-h-full flex-col">{children}</body>
+    </html>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center min-h-screen bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center">
-        <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+    <div className="flex min-h-screen flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex w-full flex-1 flex-col items-center justify-center text-center">
+        <h1 className="text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
           Root Page
         </h1>
       </main>
