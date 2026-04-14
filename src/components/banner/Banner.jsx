85a23d966acecffec6b4c9e01464025422b3ada6
sajid-islam <sajidislam941@gmail.com>
2026-04-14 19:57:01 +0600

    make responsive our specialty section for meduim to large device

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,7 +5,6 @@ import InLogo from '@/images/social-media-logos/linkedin-logo.png';
 import XLogo from '@/images/social-media-logos/twitter-logo.png';
 import Image from 'next/image';
 import OurSpecialty from './OurSpecialty';
-import ShowcaseHero from './ShowcaseHero';
 const Banner = () => {
   return (
     <div className="font-tilt-warp relative">
@@ -27,7 +26,7 @@ const Banner = () => {
       {/* Our Specialty */}
       <OurSpecialty />
       {/* Showcase */}
-      <ShowcaseHero />
+      {/* <ShowcaseHero /> */}
     </div>
   );
 };
--- src/components/banner/OurSpecialty.jsx
+++ src/components/banner/OurSpecialty.jsx
@@ -6,26 +6,26 @@ import OurSpecialtyPattern from '../polygons/our-specialty-pattern';
 
 const OurSpecialty = () => {
   return (
-    <div className="absolute top-50 ml-5">
+    <div className="absolute top-[14vw] lg:ml-6 xl:top-56 xl:ml-7">
       <OurSpecialtyPattern />
       <section>
-        <h3 className="text-2xl text-white">OUR SPECIALTY</h3>
-        <ul className="mt-5 flex flex-col gap-2 text-white">
-          <li className="flex w-full items-center gap-4 rounded-2xl border-2 border-[#c3dc9c] bg-white/15 px-5 py-3 backdrop-blur-xs">
+        <h3 className="text-white lg:text-xl xl:text-2xl">OUR SPECIALTY</h3>
+        <ul className="mt-3 flex flex-col gap-2 text-white xl:mt-5">
+          <li className="specialty-box flex w-full items-center gap-4 rounded-2xl border-2 border-[#c3dc9c] bg-white/15 backdrop-blur-xs">
             <div className="rounded-xl border-2 border-[#c3dc9c] p-2">
-              <Image src={running} alt="Running icon" className="size-12" />
+              <Image src={running} alt="Running icon" className="size-9 xl:size-12" />
             </div>
             <p>Ground Running</p>
           </li>
-          <li className="flex w-full items-center gap-4 rounded-2xl border-2 border-[#c3dc9c] bg-white/15 px-5 py-3 backdrop-blur-xs">
+          <li className="specialty-box flex w-full items-center gap-4 rounded-2xl border-2 border-[#c3dc9c] bg-white/15 backdrop-blur-xs">
             <div className="rounded-xl border-2 border-[#c3dc9c] p-2">
-              <Image src={yoga} alt="Running icon" className="size-12" />
+              <Image src={yoga} alt="Running icon" className="size-9 xl:size-12" />
             </div>
             <p>Yoga Assistance</p>
           </li>
-          <li className="flex w-full items-center gap-4 rounded-t-2xl rounded-b-[50px] border-2 border-[#c3dc9c] bg-white/15 px-5 py-6 backdrop-blur-xs">
+          <li className="specialty-box flex w-full items-center gap-4 rounded-t-2xl rounded-b-[45px] border-2 border-[#c3dc9c] bg-white/15 px-5 py-6 backdrop-blur-xs xl:rounded-b-[50px]">
             <div className="rounded-xl border-2 border-[#c3dc9c] p-2">
-              <Image src={trainer} alt="Running icon" className="size-12" />
+              <Image src={trainer} alt="Running icon" className="size-9 xl:size-12" />
             </div>
             <p>Personal Trainer</p>
           </li>
